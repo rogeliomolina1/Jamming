c7610e5d4f454fdce356d8f2cea88fa9fb689ed0
Rogelio Molina <rogelio.molina1@gmail.com>
2020-11-29 15:55:30 -0800

    Ready to implement Spotify API

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import './App.css';
-import { SearchBar } from '../SearchBar/SearchBar';
-import { SearchResults } from '../SearchResults/SearchResults';
-import { Playlist } from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-export class App extends React.Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
+      { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
+      { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
+      playlistName: 'My Playlist',
+      playlistTracks: [{ name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4 },
+      { name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5 },
+      { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6 }]
+    };
+    this.addTrack=this.addTrack.bind(this);
+    this.removeTrack=this.removeTrack.bind(this);
+    this.updatePlaylistName=this.updatePlaylistName.bind(this);
+  }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    }else{
+      tracks.push(track);
+      this.setState({playlistTracks: tracks});
+    }
+  }
+
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({playlistTracks: tracks});
+  }
+
+  updatePlaylistName(name){
+    this.setState({playlistName: name});
+  }
+
   render() {
     return (
       <div>
@@ -12,11 +48,15 @@ export class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            {console.log(this.state.searchResults)}
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}
+            onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default App;
